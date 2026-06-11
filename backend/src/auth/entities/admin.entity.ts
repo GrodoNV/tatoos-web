4423e9321cc51cfedacec93fb81e91267a8d7acb
Grodo <grodrigonvargas@gmail.com>
2026-06-11 14:45:32 -0400

    fix: permitir campos nulos en admins para sincronización

--- backend/src/auth/entities/admin.entity.ts
+++ backend/src/auth/entities/admin.entity.ts
@@ -8,10 +8,10 @@ export class Admin {
   @Column({ default: 'Admin' })
   name: string;
 
-  @Column({ unique: true })
+  @Column({ unique: true, nullable: true })
   email: string;
 
-  @Column()
+  @Column({ nullable: true })
   password: string;
 
   @Column({ default: 'employee' })
